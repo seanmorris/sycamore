20b3e3b8de7a4232f88f227a9e5897855e5c8818
Sean Morris <sean@seanmorr.is>
2021-06-05 18:48:57 -0400

    URL reference file.

--- bin/check-peer.sh
+++ bin/check-peer.sh
@@ -3,9 +3,21 @@ LATEST=0
 PEER_HOST=https://sycamore.seanmorr.is;
 PEER_FEED=2021/06/05/1622926800.sfd;
 PEER_PATH=docs/peers/@seanmorris;
+FEED_LIST=${PEER_PATH}/index.html;
 FEED_FILE=${PEER_PATH}/feeds/${PEER_FEED};
 
 mkdir -p $(dirname ${FEED_FILE});
+
+curl -s ${PEER_HOST} > ${FEED_LIST};
+
+cat ${FEED_LIST} | while read LINE; do {
+
+	FEED_BASE=$(basename $LINE);
+	FEED_TIME=${FEED_BASE%%.*};
+
+	echo $LINE " " $FEED_TIME;
+
+}; done;
 
 curl -s ${PEER_HOST}/feeds/${PEER_FEED} > ${FEED_FILE};
 
