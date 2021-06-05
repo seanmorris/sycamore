91dabf5fcac6a8254e48d666ee2cabc250f8bae8
Sean Morris <sean@seanmorr.is>
2021-06-05 19:19:36 -0400

    Getting messages in peer checker

--- bin/check-peer.sh
+++ bin/check-peer.sh
@@ -3,7 +3,6 @@ set -euo pipefail;
 LATEST=0
 
 PEER_HOST=https://sycamore.seanmorr.is;
-PEER_FEED=2021/06/05/1622926800.sfd;
 PEER_LIST=config/syndicate.txt
 
 cat ${PEER_LIST} | while read PEER_URL; do {
@@ -11,20 +10,21 @@ cat ${PEER_LIST} | while read PEER_URL; do {
 	PEER_HOST=${PEER_URL#https:\/\/}
 	PEER_PATH=docs/peers/@"${PEER_HOST}";
 	FEED_LIST=${PEER_PATH}/index.html;
-	FEED_FILE=${PEER_PATH}/feeds/${PEER_FEED};
-	FEED_PATH=$(dirname "${FEED_FILE}");
-
-	mkdir -p ${FEED_PATH};
 
 	curl ${PEER_URL} > ${FEED_LIST};
 
-	mkdir -p $(dirname ${FEED_FILE});
-	
 	cat ${FEED_LIST} | while read LINE; do {
 
 		FEED_URL=${LINE};
 		FEED_BASE=$(basename $FEED_URL);
 		FEED_TIME=${FEED_BASE%%.*};
+		PEER_FEED=${FEED_URL};
+		FEED_FILE=${PEER_PATH}/feeds/$(basename "${FEED_URL}");
+		FEED_PATH=$(dirname "${FEED_FILE}");
+		
+		mkdir -p ${FEED_PATH};
+
+		echo ${PEER_URL}/${FEED_URL} ' >>> ' ${PEER_FEED};
 
 		curl ${PEER_URL}/${FEED_URL} > ${FEED_FILE};
 		echo $FEED_URL " " $FEED_TIME;
