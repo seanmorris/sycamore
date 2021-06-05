79fb9993ad11ef22dfb8570b58c9b0593133b766
Sean Morris <sean@seanmorr.is>
2021-06-05 16:08:38 -0400

    Article edit.

--- bin/generate-post-messages.sh
+++ bin/generate-post-messages.sh
@@ -17,7 +17,7 @@ FEED_DIR=docs/feeds/`date +%Y-%m`/`date +%d`;
 
 mkdir -p $FEED_DIR;
 
-test -z $SSH_KEY && ${PRIVATE_KEY} > sycamore_key || ${SSH_KEY} > sycamore_key;
+[[ -z $SSH_KEY ]] && ${PRIVATE_KEY} > sycamore_key || ${SSH_KEY} > sycamore_key;
 
 git diff HEAD~1 --name-only content/ | while read NAME; do {
 	STATIC_HOSTNAME=${STATIC_HOSTNAME} \
