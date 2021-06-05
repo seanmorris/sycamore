090298efc0ec50c4ea42aa3f1608d38ea077dc8c
Sean Morris <sean@seanmorr.is>
2021-06-05 15:13:22 -0400

    Article edit.

--- bin/generate-latest-feed.sh
+++ bin/generate-latest-feed.sh
@@ -15,11 +15,11 @@ NOW=$(echo `date '+%s'`/${HOUR}*${HOUR} | bc);
 
 FEED_DIR=feeds/`date +%Y-%m`/`date +%d`;
 
-COMMIT=`git log -2 --format='%H' | tail -n1`;
+LASTHASH=`git log -2 --format='%H' | tail -n1`;
 
 mkdir -p $FEED_DIR;
 
-git diff ${COMMIT} --name-only content/ | while read NAME; do {
+git diff ${LASTHASH} --name-only content/ | while read NAME; do {
 
 	echo `date +%s` ${NAME#content/} >> $FEED_DIR/${NOW}.sfd;
 
