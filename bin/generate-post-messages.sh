7df7be4d16328a8226bd43c9d28f945420344cfc
Sean Morris <sean@seanmorr.is>
2021-06-05 15:35:41 -0400

    Article edit.

---
+++ bin/generate-post-messages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euxo pipefail;
+
+test -f .env && {
+	>&2 echo "Loading .env file.";
+	set -o allexport;
+	source .env;
+	set +o allexport
+} || {
+	>&2 echo "Notice: .env file not found.";
+}
+
+HOUR=$(echo '60*60' | bc);
+NOW=$(echo `date '+%s'`/${HOUR}*${HOUR} | bc);
+
+FEED_DIR=docs/feeds/`date +%Y-%m`/`date +%d`;
+
+mkdir -p $FEED_DIR;
+
+git diff HEAD~1 --name-only content/ | while read NAME; do {
+	bash bin/generate-post-message.sh ${NAME};
+}; done;
